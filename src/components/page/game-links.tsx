bb925b9bb82820066d6b1432c9bab7da46206c51
Firzus <92395888+Firzus@users.noreply.github.com>
2025-05-17 06:18:21 +0200

    fix: adjust gap spacing in GameLinks component for improved layout

--- src/components/page/game-links.tsx
+++ src/components/page/game-links.tsx
@@ -8,7 +8,7 @@ type Props = {}
 
 export const GameLinks: React.FC<Props> = () => {
   return (
-    <div className="absolute right-6 top-1/2 z-40 flex -translate-y-1/2 flex-col gap-2">
+    <div className="absolute right-6 top-1/2 z-40 flex -translate-y-1/2 flex-col gap-4">
       {socialLinks.map((link, i) => (
         <Button
           key={i}
